2a3a24a19fabd0b86817c4e25852a9833f533c8a
Nguyen Tuan Ann <nguyen.05@inbox.ru>
2024-12-27 16:28:33 +0700

    Настройка перед кодом

--- LAB5WORK/LAB5WORK.cpp
+++ LAB5WORK/LAB5WORK.cpp
@@ -1,11 +1,14 @@
 ﻿// LAB5WORK.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <cstring>
+#include <cstdlib>  // Для функций стандартного ввода
+#include <stdexcept> // Для исключений
+#include <ctime> // Для получения текущего года
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    setlocale(LC_ALL, "rus");
 }
 
-/
